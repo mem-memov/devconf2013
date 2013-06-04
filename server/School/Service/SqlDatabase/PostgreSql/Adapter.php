e2e0004ce2037ea408084face5e8b80bf29bcf64
novikov <novikov@novikov-UX31A>
2013-06-04 17:20:57 +0400

    драйвер для PostgreSQL

--- server/School/Service/SqlDatabase/PostgreSql/Adapter.php
+++ server/School/Service/SqlDatabase/PostgreSql/Adapter.php
@@ -18,5 +18,106 @@ implements
         
     }
     
+    // School_Service_SqlDatabase_AdapterInterface: START ----------------------
+
+    public function query($query) {
+        
+        $this->driver->query($query);
+        
+        return null;
+        
+    }
+
+    public function insertDefault($tableName, $idColumn, array $values = array()) {
+        
+        
+        
+    }
+
+    public function fetchRows($query) {
+        
+        
+        
+    }
+
+    public function fetchFirstRow($query) {
+        
+        
+        
+    }
+
+    public function fetchColumns($query) {
+        
+        
+        
+    }
+
+    public function fetchColumn($query, $fieldName) {
+        
+        
+        
+    }
+
+    public function fetchValueFromFirstRow($query, $fieldName) {
+        
+        
+        
+    }
+
+    public function fetchLastId() {
+        
+        
+        
+    }
+
+    public function fetchNumberOfAffectedRows() {
+        
+        
+        
+    }
+
+    public function beginTransaction() {
+        
+        
+        
+    }
+
+    public function commit() {
+        
+        
+        
+    }
+
+    public function rollback() {
+        
+        
+        
+    }
+    
+    public function prepareString($value) {
+        
+        
+        
+    }
+    
+    public function prepareBoolean($value) {
+        
+        
+        
+    }
+    
+    public function prepareIfNull($value) {
+        
+        
+        
+    }
+    
+    public function castBollean(&$value) {
+        
+        
+        
+    }
+    
+    // School_Service_SqlDatabase_AdapterInterface: STOP ----------------------
     
 }
--- server/School/Service/SqlDatabase/PostgreSql/PgsqlDriver.php
+++ server/School/Service/SqlDatabase/PostgreSql/PgsqlDriver.php
@@ -11,6 +11,9 @@ implements
     private $database;
     private $shema;
     
+    private $connection;
+    private $result;
+    
     /**
      * Создаёт экземпляр класса
      * @param string $server имя сервера
@@ -36,13 +39,126 @@ implements
         $this->database = $database;
         $this->encoding = $encoding;
         $this->shema = $shema;
+        
+        $this->connection = null;
+        $this->result = null;
 
     }
+    
     public function __destruct() {
+        
         if (is_resource($this->connection)) {
+            
             pg_close($this->connection);
+            
         }
+        
     }
     
+    /**
+     * Выполняет запрос к базе данных
+     * @param string $query
+     * @return resource
+     * @throws School_Service_SqlDatabase_Exception_QueryFailed
+     */
+    public function query($query) {
+        
+        $this->connect();
+        
+        $this->freeResult();
+
+        $this->result = @pg_query($this->connection, $query);
+        
+        if ($this->result === false) {
+            
+            throw new School_Service_SqlDatabase_Exception_QueryFailed(
+                $query, 
+                pg_last_error()
+            );
+            
+        }
+ 
+        return null;
+        
+    }
+    
+    /**
+     * Извлекает строки таблицы
+     * @param string $query SQL-запрос
+     * @return array
+     */
+    public function fetchRows($query) {
+        
+        $rows = array();
+
+        while ($row = pg_fetch_assoc($this->result)) {
+            
+            $rows[] = $row;
+            
+        }
+        
+        $this->freeResult();
+        
+        return $rows;
+
+    }
+    
+    /**
+     * Сообщает количество изменённых в последнем запросе строк
+     * @return int
+     */
+    public function fetchNumberOfAffectedRows() {
+        
+        return pg_affected_rows($this->result);
+  
+    }
+    
+    /**
+     * Выполняет подключение к базе данных
+     * @return null
+     * @throws CModel_Service_Postgresql_Exception
+     */
+    private function connect() {
+        
+        if (is_resource($this->connection)) {
+            return;
+        }
+        
+        $this->connection = pg_connect('host='.$this->server.' port='.$this->port.' dbname='.$this->database.' user='.$this->user.' password='.$this->password.' connect_timeout=1');
+        
+        if ($this->connection === false) {
+            throw new CModel_Service_Postgresql_Exception ('Не удалось подсоединиться к базе данных: ' . pg_last_error());
+        }
+
+        $this->setSchema();
+
+    }
+    
+    /**
+     * Устанавливает схему для всех последующих запросов 
+     * так, что её не нужно указывать перед именами таблиц
+     */
+    private function setSchema() {
+        
+        if (!is_null($this->shema)) {
+            $this->query('SET search_path TO '.$this->shema.',public;');
+        }
+        
+    }
+    
+    /**
+     * Очищает результат последнего запроса
+     */
+    private function freeResult() {
+        
+        if (is_resource($this->result)) {
+            
+            pg_free_result($this->result);
+            
+        }
+        
+        $this->result = null;
+        
+    }
     
 }
