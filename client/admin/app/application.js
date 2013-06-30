a3b1824e4bab4ba4968d0cc8108114d3e534b80c
novikov <novikov@novikov-UX31A>
2013-06-30 16:03:58 +0400

    в проект добавлено новое приложение - админка

---
+++ client/admin/app/application.js
@@ -0,0 +1,17 @@
+Ext.define('Admin.Application', {
+    name: 'Admin',
+
+    extend: 'Ext.app.Application',
+
+    views: [
+        // TODO: add views here
+    ],
+
+    controllers: [
+        // TODO: add controllers here
+    ],
+
+    stores: [
+        // TODO: add stores here
+    ]
+});
